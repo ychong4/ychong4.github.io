fb95c5564d7abc4885ff59b62385e1d48d8e6c06
Yaow Chong <65525074+ychong4@users.noreply.github.com>
2024-09-23 21:11:44 -0500

    Update etl.py

--- Activity 2/AWS/Airflow/Youtube_Comment_Analysis/etl.py
+++ Activity 2/AWS/Airflow/Youtube_Comment_Analysis/etl.py
@@ -10,25 +10,30 @@ def run_youtube_etl():
 	# Disable OAuthlib's HTTPS verification when running locally.
 	# *DO NOT* leave this option enabled in production.
 	os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
+	
+	
 	api_service_name = "youtube"
 	api_version = "v3"
 	DEVELOPER_KEY = "-- Enter Your Developer Key Here --"
-
+	next_page_token = None
+	
 	youtube = googleapiclient.discovery.build(
 	api_service_name, api_version, developerKey = DEVELOPER_KEY)
 
-	request = youtube.commentThreads().list(
-		part="snippet, replies",
+	while True:
+		request = youtube.commentThreads().list(
+			part="snippet, replies",
 	
-		videoId = "ndAQfTzlVjc"
-	)
+			videoId = "ndAQfTzlVjc",  # 7 Days Stranded In A Cave
+			pageToken=next_page_token,
+            		maxResults=100	
+		)
 
-	response = request.execute()
-	comments = []
-	items = response.get('items', [])
+		response = request.execute()
+		comments = []
+		items = response.get('items', [])
 	
-	for item in items:
+		for item in items:
 			author = item['snippet']['topLevelComment']['snippet']['authorDisplayName']
 			comment_text = item['snippet']['topLevelComment']['snippet']['textOriginal']
 			publish_time = item['snippet']['topLevelComment']['snippet']['publishedAt']
@@ -36,6 +41,13 @@ def run_youtube_etl():
 						'comment': comment_text, 'published_at': publish_time}
 
 			comments.append(comment_info)
+		next_page_token = comment_response.get('nextPageToken')
+        	if not next_page_token:
+            		break
+
+	return comments
+
+def to_csv(comments):
 	df = pd.DataFrame(comments)
 	df.to_csv("s3://myairflowyoutubebucket/comments.csv")
 
