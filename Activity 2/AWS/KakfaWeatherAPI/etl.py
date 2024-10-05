571034631f3f405f85e46cf62891dd6c5aff6189
ychong4 <ychong@unomaha.edu>
2024-10-05 00:09:52 -0500

    init

---
+++ Activity 2/AWS/KakfaWeatherAPI/etl.py
@@ -0,0 +1,33 @@
+import requests
+from key import api_key
+
+def get_weather(lat, lon, api_key):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "lat": lat,
+        "lon": lon,
+        "appid": api_key,
+        "units": "metric"  # For temperature in Celsius
+    }
+
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+lat = 40.806862
+lon = -96.681679
+api_key = api_key
+
+weather_data = get_weather(lat, lon, api_key)
+
+
+if weather_data:
+    print(f"Temperature: {weather_data['main']['temp']}°C")
+    print(f"Description: {weather_data['weather'][0]['description']}")
+    print(f"Humidity: {weather_data['main']['humidity']}%")
+else:
+    print("Failed to retrieve weather data.")
