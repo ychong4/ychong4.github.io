5f6bf75a2b328072232ac20a77caa78279409c20
Yaow Chong <65525074+ychong4@users.noreply.github.com>
2024-11-02 20:49:26 -0500

    Delete Activity 2/SentimentWebsite/hftoken.py

--- Activity 2/SentimentWebsite/hftoken.py
+++
@@ -1,5 +0,0 @@
-huggingface_token = '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'
-
-secret_name = "rds!db-cdacc544-7d3a-45bc-92d1-7929cb3fb80b"
-region_name = "us-east-2"
-host = "db-stock.cbueaqgy4mwk.us-east-2.rds.amazonaws.com"
